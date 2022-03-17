19e61a835396df24d6a27e78bf45072f967487ed
Vladimir Myltsin <vova.myltsin@yandex.ru>
2022-03-17 12:24:40 +0400

    init routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import styles from './App.module.css';
+import {Route, Routes, Link} from "solid-app-router";
+import {lazy} from "solid-js";
+
+const Users = lazy(() => import('./pages/Users'));
+const Home = lazy(() => import('./pages/Home'));
+const NotFound = lazy(() => import('./pages/NotFound'));
 
 function App() {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
+  return (<div class={styles.App}>
+    <h1>Awesome Site</h1>
+    <Link class="nav" href="/">
+      Home
+    </Link>
+    <Link class="nav" href="/users">
+      Users
+    </Link>
+    <Routes>
+      <Route path="/users" element={<Users />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/*all" element={<NotFound />} />
+    </Routes>
+  </div>);
 }
 
 export default App;
